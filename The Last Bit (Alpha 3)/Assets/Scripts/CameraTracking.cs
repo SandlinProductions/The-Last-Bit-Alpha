74ad6527d4cf5aa8d9d58b2fa03456117b162049
Stephen Sandlin <info@SandlinProds.com>
2019-04-28 13:46:59 -0400

    Initial gitHub Set Up
    Uploading Project to gitHub

---
+++ The Last Bit (Alpha 3)/Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    [SerializeField]
+    Transform trackingTarget;
+    [SerializeField]
+    float xOffset;
+    [SerializeField]
+    float yOffset;
+    [SerializeField]
+    float followSpeed;
+    public PlayerController playerController;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (playerController.facingRight == true)
+        {
+            float xTarget = trackingTarget.position.x + xOffset;
+            float yTarget = trackingTarget.position.y + yOffset;
+
+            float xNew = Mathf.Lerp(transform.position.x, xTarget, Time.deltaTime * followSpeed);
+            float yNew = Mathf.Lerp(transform.position.y, yTarget, Time.deltaTime * followSpeed);
+
+            transform.position = new Vector3(xNew, yNew, transform.position.z);
+        }
+        else if (playerController.facingRight == false)
+        {
+            float xTarget = trackingTarget.position.x + -xOffset;
+            float yTarget = trackingTarget.position.y + yOffset;
+
+            float xNew = Mathf.Lerp(transform.position.x, xTarget, Time.deltaTime * followSpeed);
+            float yNew = Mathf.Lerp(transform.position.y, yTarget, Time.deltaTime * followSpeed);
+
+            transform.position = new Vector3(xNew, yNew, transform.position.z);
+        }
+
+    }
+}
---
+++ The Last Bit (Alpha 3)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //This scripts holds everything that's going on with the player
+    public bool finshed;
+    public bool grounded;
+    public bool candoublejump;
+    public bool facingRight;// The Checkbox that tells which way the player is facing
+    public bool alive;// The Checkbox that is to tell if the player is alive or not.
+
+    void Start()
+    {
+        finshed = false;
+        alive = true;
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //Check if Grounded
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        grounded = true;
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
+    }
+}
