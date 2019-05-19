34c5a380f04a939cc356205f44a96082034ea4ad
Stephen Sandlin <info@SandlinProds.com>
2019-05-19 09:04:23 -0400

    Scripts and Enemy
    Added Partol and Abilities Scripts. Added Placement for Enemy for Level One

---
+++ The Last Bit (Alpha 3)/Assets/Scripts/PartolController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartolController : MonoBehaviour
+{
+    //This script is for anything we want to move from one point to other point(s) then back again.
+    public Transform[] patrolPoints;
+    public float moveSpeed;
+    private int currentPoint;
+
+    void Start()
+    {
+        transform.position = patrolPoints[0].position;
+        currentPoint = 0;
+    }
+
+    void FixedUpdate()
+    {
+
+        if (transform.position == patrolPoints[currentPoint].position)
+        {
+            currentPoint++;
+        }
+        if (currentPoint >= patrolPoints.Length)
+        {
+            currentPoint = 0;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position, moveSpeed * Time.deltaTime);
+
+    }
+}  
