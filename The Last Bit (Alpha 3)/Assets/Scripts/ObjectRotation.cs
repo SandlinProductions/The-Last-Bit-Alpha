c1c1dc13c57aaf4b50564a9081853096f11e62b7
Stephen Sandlin <info@SandlinProds.com>
2019-05-20 11:03:58 -0400

    Object Rotation
    Added Object Rotation Script and Added it to the Enemy Prefab

---
+++ The Last Bit (Alpha 3)/Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+    //This Script is used to rotate objects over time
+    [Header("Object Rotation")]
+    [SerializeField]
+    [Tooltip("This will change the speed of the rotation on X,Y,Z")]
+    private Vector3 rotationVelocity;//the rotation of the object over x,y,z
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationVelocity * Time.deltaTime);
+    }
+}
