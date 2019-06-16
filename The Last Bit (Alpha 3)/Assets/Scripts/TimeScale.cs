c49ab70e3d1ee3d7d62e08208ec5b8b1806c46c5
Stephen Sandlin <info@SandlinProds.com>
2019-06-16 12:47:13 -0400

    Pause Menu and TMP
    Added a Pause Menu and added in TMP  to the project. Made a input for the D-pad

--- The Last Bit (Alpha 3)/Assets/Scripts/TimeScale.cs
+++ The Last Bit (Alpha 3)/Assets/Scripts/TimeScale.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class TimeScale : MonoBehaviour
 {
+  
+    public Abilities abilities;
 
+    private void Awake()
+    {
+        abilities = FindObjectOfType<Abilities>();
+    }
     //This is how we slow it all down.
     void Update()
     {
-        if (Input.GetButtonDown("Time") & PauseMenu.GameIsPaused == false)
+        if (abilities.GetComponent<Abilities>().timeShift == true)
         {
-            if (Time.timeScale == 1.0F)
-                Time.timeScale = 0.2F;
-            else
-                Time.timeScale = 1.0F;
-            Time.fixedDeltaTime = 0.02F * Time.timeScale;
+            if (Input.GetButtonDown("Time") & PauseMenu.GameIsPaused == false)
+            {
+                if (Time.timeScale == 1.0F)
+                    Time.timeScale = 0.2F;
+                else
+                    Time.timeScale = 1.0F;
+                Time.fixedDeltaTime = 0.02F * Time.timeScale;
+            }
         }
     }
 }
